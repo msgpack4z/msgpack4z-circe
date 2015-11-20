e20f43fec90593b43ec59c024f01216f076c950c
xuwei-k <6b656e6a69@gmail.com>
2015-11-21 02:30:43 +0900

    sbt 0.13.9

--- project/Common.scala
+++ project/Common.scala
@@ -24,6 +24,7 @@ object Common {
     sonatypeSettings
   ).flatten ++ Seq(
     resolvers += Opts.resolver.sonatypeReleases,
+    fullResolvers ~= {_.filterNot(_.name == "jcenter")},
     commands += Command.command("updateReadme")(UpdateReadme.updateReadmeTask),
     releaseProcess := Seq[ReleaseStep](
       ReleaseStep{ state =>
