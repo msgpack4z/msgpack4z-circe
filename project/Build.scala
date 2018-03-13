0fcd9b48e94d4193f9bad9afb860ce3671cf3974
kenji yoshida <6b656e6a69@gmail.com>
2018-03-13 23:59:07 +0900

    circe 0.9.2

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object build {
     scalapropsCoreSettings,
     name := msgpack4zCirceName,
     libraryDependencies ++= (
-      ("io.circe" %%% "circe-core" % "0.9.1") ::
+      ("io.circe" %%% "circe-core" % "0.9.2") ::
       ("com.github.xuwei-k" %%% "msgpack4z-core" % "0.3.8") ::
       ("com.github.scalaprops" %%% "scalaprops" % "0.5.3" % "test") ::
       ("com.github.xuwei-k" %%% "msgpack4z-native" % "0.3.4" % "test") ::
