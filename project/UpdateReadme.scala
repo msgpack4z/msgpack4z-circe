c2b551aa065602804b0de8e589e237810e26d691
xuwei-k <6b656e6a69@gmail.com>
2019-09-17 17:01:45 +0900

    use 2.12 for sxr

--- project/UpdateReadme.scala
+++ project/UpdateReadme.scala
@@ -8,7 +8,7 @@ object UpdateReadme {
 
   val updateReadmeTask = { state: State =>
     val extracted = Project.extract(state)
-    val scalaV = "2.11"
+    val scalaV = "2.12"
     val v = extracted get version
     val org = extracted get organization
     val modules = build.modules
