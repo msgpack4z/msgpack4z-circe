b6717244d60add3e85ae650453029d2d48a66ce5
xuwei-k <6b656e6a69@gmail.com>
2023-12-25 14:11:56 +0900

    add `-Xsource:3`

--- jvm/src/test/scala/msgpack4z/JavaSpec.scala
+++ jvm/src/test/scala/msgpack4z/JavaSpec.scala
@@ -1,6 +1,6 @@
 package msgpack4z
 
 object JavaSpec extends SpecBase {
-  override protected[this] def packer() = new MsgpackJavaPacker()
+  override protected[this] def packer(): MsgPacker = new MsgpackJavaPacker()
   override protected[this] def unpacker(bytes: Array[Byte]) = MsgpackJavaUnpacker.defaultUnpacker(bytes)
 }
